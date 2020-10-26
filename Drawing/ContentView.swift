0c01c34887112e25db9414cdc9cf561e32699432
Dobuzi <kimjw7981@gmail.com>
2020-10-26 21:31:17 +0900

    Arc

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,10 +7,60 @@
 
 import SwiftUI
 
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        
+        return path
+    }
+}
+
+struct Arc: InsettableShape {
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool
+    var insetAmount: CGFloat = 0
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        let rotationAdjustment = Angle.degrees(90)
+        let modifiedStartAngle = startAngle - rotationAdjustment
+        let modifiedEndAngle = endAngle - rotationAdjustment
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStartAngle, endAngle: modifiedEndAngle, clockwise: !clockwise)
+        
+        return path
+    }
+    
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var arc = self
+        arc.insetAmount += amount
+        return arc
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Triangle()
+                .stroke(
+                    Color.red.opacity(0.5),
+                    style: StrokeStyle(
+                        lineWidth: 10,
+                        lineCap: .round,
+                        lineJoin: .round
+                    )
+                )
+                .frame(width: 300, height: 300)
+            Spacer()
+            Arc(startAngle: .degrees(0), endAngle: .degrees(110), clockwise: true)
+                .strokeBorder(Color.blue, lineWidth: 10)
+                .frame(width: 300, height: 300)
+        }
     }
 }
 
