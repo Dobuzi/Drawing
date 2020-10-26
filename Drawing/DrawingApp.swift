cb9a0f2de820e435b911898fad1b66967d2b017d
Dobuzi <kimjw7981@gmail.com>
2020-10-26 13:22:55 +0900

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by 김종원 on 2020/10/26.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
