65e97d028029295d3a0722f3ec825f2de187ee44
Dobuzi <kimjw7981@gmail.com>
2020-10-27 13:53:32 +0900

    Trapezoid

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,35 +7,40 @@
 
 import SwiftUI
 
+struct Trapezoid: Shape {
+    var insetAmount: CGFloat
+    
+    var animatableData: CGFloat {
+        get { insetAmount }
+        set { self.insetAmount = newValue }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+        
+        return path
+    }
+}
 
 struct ContentView: View {
-    @State private var amount: CGFloat = 1.0
+    @State private var amount: CGFloat = 50
     var body: some View {
         VStack {
-            ZStack {
-                Circle()
-                    .fill(Color(red: 1, green: 0, blue: 0))
-                    .frame(width: 200 * amount)
-                    .offset(x: -50, y: -80)
-                    .blendMode(.screen)
-                Circle()
-                    .fill(Color(red: 0, green: 1, blue: 0))
-                    .frame(width: 200 * amount)
-                    .offset(x: 50, y: -80)
-                    .blendMode(.screen)
-                Circle()
-                    .fill(Color(red: 0, green: 0, blue: 1))
-                    .frame(width: 200 * amount)
-                    .blendMode(.screen)
-            }
-            .frame(width: 300, height: 300)
-            
-            Slider(value: $amount)
-                .padding()
+            Trapezoid(insetAmount: amount)
+                .frame(width: 200, height: 100)
+                .onTapGesture(count: 1, perform: {
+                    withAnimation {
+                        self.amount = CGFloat.random(in: 10...90)
+                    }
+                })
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.black)
-        .ignoresSafeArea()
+        .padding()
     }
 }
 
