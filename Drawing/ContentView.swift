dc3590c6ed18c84c898ed95cc753ef005f550963
Dobuzi <kimjw7981@gmail.com>
2020-10-27 21:06:07 +0900

    Arrow

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,100 +7,34 @@
 
 import SwiftUI
 
-struct Spirograph: Shape {
-    let innerRadius: Int
-    let outerRadius: Int
-    let distance: Int
-    let amount: CGFloat
-    
-    func gcd(_ a: Int, _ b: Int) -> Int {
-        var a = a
-        var b = b
-        
-        while b != 0 {
-            let temp = b
-            b = a % b
-            a = temp
-        }
-        
-        return a
-    }
+struct Arrow: Shape {
+    let thickness: CGFloat
     
     func path(in rect: CGRect) -> Path {
-        let divisor = gcd(innerRadius, outerRadius)
-        let outerRadius = CGFloat(self.outerRadius)
-        let innerRadius = CGFloat(self.innerRadius)
-        let distance = CGFloat(self.distance)
-        let difference = innerRadius - outerRadius
-        let endPoint = ceil(2 * CGFloat.pi * outerRadius / CGFloat(divisor)) * amount
-        
         var path = Path()
         
-        for theta in stride(from: 0, through: endPoint, by: 0.01) {
-            var x = difference * cos(theta) + distance * cos(difference / outerRadius * theta)
-            var y = difference * sin(theta) - distance * sin(difference / outerRadius * theta)
-            
-            x += rect.width / 2
-            y += rect.height / 2
-            
-            if theta == 0 {
-                path.move(to: CGPoint(x: x, y: y))
-            } else {
-                path.addLine(to: CGPoint(x: x, y: y))
-            }
-        }
+        path.addRect(CGRect(x: rect.midX - thickness * 2, y: rect.midY - thickness/2, width: thickness * 2, height: thickness))
+        path.move(to: CGPoint(x: rect.midX, y: rect.midY + thickness))
+        path.addLine(to: CGPoint(x: rect.midX + 1.5 * thickness, y: rect.midY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.midY - thickness))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.midY + thickness))
         
         return path
     }
 }
 
 struct ContentView: View {
-    @State private var innerRadius = 125.0
-    @State private var outerRadius = 75.0
-    @State private var distance = 25.0
-    @State private var amount: CGFloat = 1.0
-    @State private var hue = 0.6
+    @State private var thickness: CGFloat = 50.0
     var body: some View {
         VStack {
+            Arrow(thickness: thickness)
             Spacer()
-            Spirograph(innerRadius: Int(innerRadius), outerRadius: Int(outerRadius), distance: Int(distance), amount: amount)
-                .stroke(Color(hue: hue, saturation: 1, brightness: 1), lineWidth: 1)
-                .frame(width: 400, height: 400)
-            Spacer()
-            Group {
-                HStack {
-                    Text("Inner radius: \(Int(innerRadius))")
-                    Slider(value: $innerRadius, in: 10...150, step: 1)
-                        
-                }
-                .padding([.horizontal, .bottom])
-                
-                HStack {
-                    Text("Outer radius: \(Int(outerRadius))")
-                    Slider(value: $outerRadius, in: 10...150, step: 1)
-                }
-                .padding([.horizontal, .bottom])
-                
-                HStack {
-                    Text("Distance: \(Int(distance))")
-                    Slider(value: $distance, in: 1...150, step: 1)
-                }
-                .padding([.horizontal, .bottom])
-                
-                HStack {
-                    Text("Amount: \(amount, specifier: "%.2f")")
-                    Slider(value: $amount)
-                }
-                .padding([.horizontal, .bottom])
-                
-                HStack {
-                    Text("Color")
-                    Slider(value: $hue)
-                }
-                .padding(.horizontal)
+            HStack {
+                Text("Thickness")
+                Slider(value: $thickness, in: 5...100)
             }
+            .padding()
         }
-        
     }
 }
 
